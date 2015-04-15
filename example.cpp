f480b97b7219f0db8788299e40331eaef14e39d0
ptendolkar <pt39@njit.edu>
2015-04-15 13:40:54 -0400

    generalize example

--- example.cpp
+++ example.cpp
@@ -43,27 +43,31 @@ int main(int argc, char *argv[])
 	int devID = 0;	
 	initializeCUDA(argc, argv, devID);
 	
-//	double h_A[9] = {2.0, 0.0, 0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 2.0};
-//	double h_B[9] = {2.0, 3.0, 4.0, 2.0, 3.0, 4.0, 2.0, 3.0, 4.0};
-	
 	Matrix h_A(3,3);
-	Matrix h_B(3,3,2);
-
+	Matrix h_B(3,3);
+	
 	Matrix h_C(3,3);
-
+	
 	h_A.identity();
+	h_B.initialize();
+
+	std::cout << "A:" << std::endl;
+	h_A.print();
+
+	std::cout << "B:" << std::endl;
+	h_B.print();
 	
     	double *d_A, *d_B, *d_C;
 	
 	// allocate host memory for matrices A and B
-   	unsigned int size_A = 9; 
-   	unsigned int mem_size_A = sizeof(double) * size_A;
+   	size_t size_A = h_A.nrow() * h_A.ncol(); 
+   	size_t mem_size_A = sizeof(double) * size_A;
     	 
-	unsigned int size_B = 9;
-    	unsigned int mem_size_B = sizeof(double) * size_B;
+	size_t size_B = h_B.nrow() *h_B.ncol();
+    	size_t mem_size_B = sizeof(double) * size_B;
 
-	unsigned int mem_size_C = mem_size_A;
-	double *h_CUBLAS = (double *) malloc(mem_size_C);
+	size_t size_C = h_A.nrow()*h_B.ncol();
+	size_t mem_size_C = sizeof(double) * size_C;
 
 	checkCudaErrors(cudaMalloc((void **) &d_A, mem_size_A));
     	checkCudaErrors(cudaMalloc((void **) &d_B, mem_size_B));
@@ -89,12 +93,8 @@ int main(int argc, char *argv[])
 
 	std::cout << "output" << std::endl;
 	
-/*	for (int i = 0; i < 9; i++)
-		std::cout << h_CUBLAS[i] << " ";
-	std::cout << std::endl;
-*/
 	h_C.print();
 	
-	std::cout << "test successful" << std::endl;
 	return 0;	
 }
+
